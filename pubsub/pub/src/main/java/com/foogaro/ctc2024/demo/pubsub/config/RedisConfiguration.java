e4a163b8bebca10d5b572b48391a80e5f1b9d96f
foogaro <l.fugaro@gmail.com>
2024-09-02 11:38:08 +0200

    Reduced the Docker image of 2 third, and adjusted the Redis configuration with better uid and pwd management

--- pubsub/pub/src/main/java/com/foogaro/ctc2024/demo/pubsub/config/RedisConfiguration.java
+++ pubsub/pub/src/main/java/com/foogaro/ctc2024/demo/pubsub/config/RedisConfiguration.java
@@ -3,10 +3,12 @@ package com.foogaro.ctc2024.demo.pubsub.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.ObjectUtils;
 import redis.clients.jedis.Jedis;
 
 @Configuration
@@ -16,32 +18,18 @@ public class RedisConfiguration {
     private String redisHost;
     @Value("${spring.data.redis.port}")
     private int redisPort;
+    @Value("${spring.data.redis.username}")
+    private String redisUsername;
     @Value("${spring.data.redis.password}")
     private String redisPassword;
 
     @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisHost, redisPort);
-        JedisConnectionFactory factory = new JedisConnectionFactory(configuration);
-        return factory;
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        final RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        return template;
-    }
-
-    @Bean
-    public HashOperations hashOperations() {
-        return redisTemplate().opsForHash();
-    }
-
-    @Bean
     public Jedis jedis() {
         Jedis jedis = new Jedis(redisHost, redisPort);
-        jedis.auth(redisPassword);
+        if (!ObjectUtils.isEmpty(redisPassword)) {
+            if (ObjectUtils.isEmpty(redisUsername)) redisUsername = "default";
+            jedis.auth(redisUsername,redisPassword);
+        }
         return jedis;
     }
 }
